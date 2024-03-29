e4cdb079091e7b2effd17af09fb08c5438285695
Israt85 <isratanjuman3@gmail.com>
2024-03-29 21:37:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const app = express()
+const cors= require('cors')
+require('dotenv').config()
+const port = 3000
+
+
+app.use(cors())
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rqq4klv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const database= client.db('TrakingDB')
+    const taskCollection = database.collection('TaskCollection')
+    const userCollection = database.collection('UserCollection')
+
+
+    app.post('/users', async(req,res)=>{
+      const user = req.body
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query)
+      if (existingUser) {
+          return res.send({ message: 'user already in', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
+
+    app.get('/tracking', async(req,res)=>{
+      const result = await taskCollection.find().toArray()
+      res.send(result)
+    })
+
+    
+   app.post('/tracking', async(req,res)=>{
+    const task = req.body;
+    const result= await taskCollection.insertOne(task)
+    res.send(result)
+   })
+
+app.put('/tracking/:id', async(req,res)=>{
+  const id = req.params.id
+  
+  const updatedTask = req.body
+
+   const filter = {_id : new ObjectId(id)}
+   const update= {
+      $set:{
+          ...updatedTask
+      }
+   }
+   const result = await taskCollection.updateOne(filter,update,{
+      upsert: true
+   })
+   res.send(result)
+})
+
+
+
+
+   app.delete('/tracking/:id', async(req,res)=>{
+  const id = req.params.id;
+  const query= {_id : new ObjectId(id)}
+  const result = await taskCollection.deleteOne(query)
+  res.send(result)
+
+   })
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
